c415e0c376a8d38845c8529b9c540d1bccdcc13f
ananya <lakshmiananya07@gmail.com>
2026-04-01 09:32:43 +0530

    Use Case 2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,80 @@
-public class UseCase1HotelBookingApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay");
-        System.out.println("Hotel Booking System v1.0");
+abstract class Room {
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
     }
+
+    public abstract void displayRoomDetails();
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("=== Hotel Room Details ===\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable + "\n");
+
+        System.out.println("Application Finished.");
+    }
+
 }
