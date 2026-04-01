d37f90ee4ba3c1f3d294228d72f016f9affbadc2
ananya <lakshmiananya07@gmail.com>
2026-04-01 09:46:54 +0530

    Use Case 4: Room Search & Availability Check

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,50 +1,25 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
 
-class RoomInventory {
-
-    private Map<String, Integer> inventory;
-
-    public RoomInventory() {
-        inventory = new HashMap<>();
-        inventory.put("Single Room", 5);
-        inventory.put("Double Room", 3);
-        inventory.put("Suite Room", 2);
-    }
-
-    public int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
-    }
-
-    public void updateAvailability(String roomType, int count) {
-        inventory.put(roomType, count);
-    }
-
-    public void displayInventory() {
-        System.out.println("Current Room Inventory:");
-        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-    }
-}
-
-public class UseCase3InventorySetup {
-
+public class TrainConsistUC4 {
     public static void main(String[] args) {
 
-        RoomInventory inventory = new RoomInventory();
+        LinkedList<String> train = new LinkedList<>();
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
 
-        inventory.displayInventory();
+        System.out.println("Initial Train Consist:");
+        System.out.println(train);
+        train.add(2, "Pantry");
 
-        System.out.println("\nChecking availability:");
-        System.out.println("Single Room Available: " + inventory.getAvailability("Single Room"));
-
-        System.out.println("\nUpdating availability...");
-        inventory.updateAvailability("Single Room", 4);
-
-        System.out.println("\nUpdated Inventory:");
-        inventory.displayInventory();
-
+        System.out.println("\nAfter adding Pantry at position 2:");
+        System.out.println(train);
+        train.removeFirst();
+        train.removeLast();
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(train);
     }
 
 }
